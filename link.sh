9f168d6b681ec26d7f29be00657550a668efe258
LazyGreed <kosalseng3010@gmail.com>
2025-11-13 18:46:08 +0700

    CHANGES: re-added hypridle, removed swayidle; update link.sh script; added Wallpapers folder

--- link.sh
+++ link.sh
@@ -11,3 +11,26 @@ for folder in ~/minimal-niri/.config/*/; do
     echo "Creating symlink: $folder_name"
     ln -sv "$folder" ~/.config/"$folder_name"
 done
+
+wallpaper_source=$HOME/minimal-niri/Wallpapers
+wallpaper_dest=$HOME/Pictures/Wallpapers
+if [ -d "$wallpaper_dest" ]; then
+    echo "Wallpapers destination folder exist. Copying content..."
+    cp -v "$wallpaper_source"/* "$wallpaper_dest"
+    echo "Wallpapers copied."
+else
+    echo "'$wallpaper_dest' not found. Creating it now..."
+    mkdir -p "$wallpaper_source"
+
+    if [ $? -eq 0 ]; then
+        echo "'$wallpaper_dest' created successfully. Copying content..."
+        cp -r "$wallpaper_source"/* "$wallpaper_dest"
+        echo "Wallpapers copied."
+    else
+        echo "Error in creating '$wallpaper_dest'."
+        exit 1
+    fi
+fi
+
+echo "Script finished!!!"
+
